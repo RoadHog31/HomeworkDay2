37402f692fa0d7b199c2ca372a03bb9b2cab061a
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-09 16:01:14 +0000

    Add project files.

---
+++ HomeworkDay2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader Reader = new StreamReader("DecodeThis.txt");
+            // Compose a string that consists of the file.
+            string line = "";
+
+            System.IO.StreamWriter newFile = new System.IO.StreamWriter("WriteThis.txt");
+
+            Console.WriteLine("Starting......!");
+
+            while (line != null)
+            {
+                line = Reader.ReadLine();
+                if (line != null)
+                {
+                    char[] charArray = line.ToCharArray();
+                    Array.Reverse(charArray);
+                    //Console.WriteLine(charArray);
+
+                    // Write the string to a file.                    
+                    newFile.WriteLine(charArray);
+                 }                
+            }
+            Reader.Close();
+            newFile.Close();
+
+            Console.WriteLine("......Finished!");
+            Console.ReadLine();         
+            //file.Close();                                  
+         }
+    }
+}
